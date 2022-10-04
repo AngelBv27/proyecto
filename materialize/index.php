847bc4f47ab36ba9b7c0cdc84ef144f8fe9738c1
AngelBv27 <114432709+AngelBv27@users.noreply.github.com>
2022-10-03 19:25:42 -0500

    Add files via upload

---
+++ materialize/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+  <title>Página principal</title>
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+</head>
+<body>
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Inicio</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Información</a></li>
+        <li><a href="badges.html">Imagenes</a></li>
+        <li><a href="collapsible.html">Extra</a></li>
+      </ul>
+    </div>
+  </nav>
+  <h1 class="center black-text">Animales</h1>
+
+<img src="img/anim1.jpg">
+<img src="img/anim2.jpg">
+<img src="img/anim3.jpg">
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+</body>
+
+</html>
